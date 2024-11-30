f672b95b05ab6a7f010dcb6f2af032172478f0e3
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2024-11-30 12:58:39 +0100

    Prompt before overwriting data

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ mod stacktraces;
 mod tracker;
 
 use crate::tracker::Tracker;
+use anyhow::bail;
 use clap::builder::styling::AnsiColor;
 use clap::builder::Styles;
 use clap::Parser;
+use dialoguer::theme::ColorfulTheme;
+use dialoguer::Confirm;
 use log::{debug, info};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::thread;
 use std::time::Duration;
 
@@ -42,13 +45,7 @@ fn main() -> anyhow::Result<()> {
     let sample_sleep_duration = Duration::from_millis(args.sample_rate.unwrap_or(1000));
 
     std::fs::create_dir_all(&args.output_dir)?;
-    for file in std::fs::read_dir(&args.output_dir)? {
-        let file = file?;
-        if file.file_name().to_string_lossy().ends_with(".json") {
-            debug!("Removing old file {:?}", file.path());
-            std::fs::remove_file(file.path())?;
-        }
-    }
+    clear_data_dir(&args.output_dir)?;
 
     let mut tracker = Tracker::new(args.pid, args.output_dir, args.native)?;
     while tracker.is_still_tracking() {
@@ -57,6 +54,38 @@ fn main() -> anyhow::Result<()> {
     }
 
     info!("All processes have exited, exiting");
+
+    Ok(())
+}
+
+fn clear_data_dir(dir: &Path) -> anyhow::Result<()> {
+    let mut files = Vec::new();
+    for file in std::fs::read_dir(dir)? {
+        let file = file?;
+        if file.file_name().to_string_lossy().ends_with(".json") {
+            files.push(file);
+        }
+    }
+
+    let file_names = files
+        .iter()
+        .map(|f| f.path().to_string_lossy().to_string())
+        .collect::<Vec<_>>();
+    let file_names = file_names.join(", ");
+
+    let confirm = Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt(format!("Are you sure you want to delete {}?", file_names))
+        .default(false)
+        .interact()?;
+
+    if !confirm {
+        bail!("User cancelled deletion");
+    }
+
+    for file in files {
+        debug!("Removing old file {:?}", file.path());
+        std::fs::remove_file(file.path())?;
+    }
 
     Ok(())
 }
