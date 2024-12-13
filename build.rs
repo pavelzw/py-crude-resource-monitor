5dea4fc1ead17cd351f21b87288d272915808928
Pavel Zwerschke <pavelzw@gmail.com>
2024-12-13 14:12:38 +0100

    .

--- build.rs
+++ build.rs
@@ -1,13 +1,17 @@
 use std::process::Command;
 
 fn main() {
-    Command::new("pnpm")
+    #[cfg(not(windows))]
+    let pnpm_executable = "pnpm";
+    #[cfg(windows)]
+    let pnpm_executable = "pnpm.cmd";
+    Command::new(pnpm_executable)
         .args(["install"])
         .current_dir("frontend")
         .status()
         .expect("Failed to run pnpm install");
 
-    Command::new("pnpm")
+    Command::new(pnpm_executable)
         .args(["build"])
         .current_dir("frontend")
         .status()
