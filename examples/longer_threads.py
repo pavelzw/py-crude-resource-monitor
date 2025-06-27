2b87a84a464ce1555915d2fb50b41ae8b5f41f8b
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2025-06-27 11:43:32 +0200

    Add multi thread example

---
+++ examples/longer_threads.py
@@ -0,0 +1,21 @@
+import threading
+
+def _dummy():
+    sum = 0
+    for j in range(0, 10):
+        for i in range(0, 1_000_000):
+            sum += 1
+    print(sum)
+
+for index in range(0, 20):
+    if index % 10 == 0:
+        print("Iteration: ", index)
+    ts = []
+    for i in range(0, 2):
+        t = threading.Thread(target=_dummy)
+        ts.append(t)
+        t.start()
+    for t in ts:
+        t.join()
+
+print("HEY")
